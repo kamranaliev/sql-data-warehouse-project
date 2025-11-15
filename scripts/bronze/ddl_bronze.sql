86cff4b8a46358e2651b209838e21dc9a92f59b1
kamranaliev <kamranaliev2@gmail.com>
2025-11-16 00:27:55 +0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -4,13 +4,14 @@ DDL Script: Create Bronze Tables
 ===============================================================================
 */
 
-CREATE TABLE bronze.crm_cust_info(
-cst_id INT,
-cst_key VARCHAR(50),
-cst_firstname VARCHAR(50),
-cst_lastname VARCHAR(50),
-cst_material_status VARCHAR(50),
-crt_create_date DATE
+create TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE
 );
 
 CREATE TABLE bronze.crm_prd_info (
