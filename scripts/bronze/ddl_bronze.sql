ca85c005418826045c977273a615af8412fb6452
kamranaliev <kamranaliev2@gmail.com>
2025-11-16 21:32:54 +0400

    ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,7 +2,12 @@
 ===============================================================================
 DDL Script: Create Bronze Tables
 ===============================================================================
-*/ 
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
@@ -17,45 +22,45 @@ CREATE TABLE bronze.crm_cust_info (
 
 DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      VARCHAR(50),
-    prd_nm       VARCHAR(50),
-    prd_cost     INT,
-    prd_line     VARCHAR(50),
-    prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP
+    prd_id       		INT,
+    prd_key      		VARCHAR(50),
+    prd_nm       		VARCHAR(50),
+    prd_cost     		INT,
+    prd_line     		VARCHAR(50),
+    prd_start_dt 		TIMESTAMP,
+    prd_end_dt   		TIMESTAMP
 );
 
 DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  VARCHAR(50),
-    sls_prd_key  VARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+    sls_ord_num  		VARCHAR(50),
+    sls_prd_key  		VARCHAR(50),
+    sls_cust_id  		INT,
+    sls_order_dt 		INT,
+    sls_ship_dt  		INT,
+    sls_due_dt  		INT,
+    sls_sales    		INT,
+    sls_quantity 		INT,
+    sls_price   		INT
 );
 
 DROP TABLE IF EXISTS bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
-    cid    VARCHAR(50),
-    cntry  VARCHAR(50)
+    cid    				VARCHAR(50),
+    cntry  				VARCHAR(50)
 	);
 
 DROP TABLE IF EXISTS bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
-    cid    VARCHAR(50),
-    bdate  DATE,
-    gen    VARCHAR(50)
+    cid    				VARCHAR(50),
+    bdate  				DATE,
+    gen    				VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           VARCHAR(50),
-    cat          VARCHAR(50),
-    subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
+    id           		VARCHAR(50),
+    cat         		VARCHAR(50),
+    subcat       		VARCHAR(50),
+    maintenance  		VARCHAR(50)
 );
