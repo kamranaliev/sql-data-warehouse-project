6cb6c6b2285577fda849ed65d42b4930c92d3b09
kamranaliev <kamranaliev2@gmail.com>
2025-11-17 16:19:42 +0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -55,3 +55,46 @@ END AS prd_line,
 CAST(prd_start_dt AS DATE) AS prd_start_dt,
 CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 DAY' AS DATE)   AS prd_end_dt
 FROM bronze.crm_prd_info;
+
+=======================================================
+
+INSERT INTO silver.crm_sales_details (
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+)
+SELECT 
+sls_ord_num, 
+sls_prd_key, 
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS VARCHAR)) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+
+CASE WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS VARCHAR)) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt, 
+
+CASE WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS VARCHAR)) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt, 
+
+CASE WHEN sls_sales <= 0 OR sls_sales != ABS(sls_price) * ABS(sls_quantity) OR sls_sales IS NULL
+	 THEN ABS(sls_price) * ABS(sls_quantity)
+	 ELSE sls_sales
+END AS sls_sales,
+
+sls_quantity,
+
+CASE WHEN sls_price <= 0  OR sls_price IS NULL OR sls_price != ABS(sls_sales) / NULLIF(sls_quantity,0)
+	 THEN ABS(sls_sales) / NULLIF(sls_quantity,0)
+	 ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
+
