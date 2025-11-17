0ed0612441766511b114aacb292e74d5fff57cec
kamranaliev <kamranaliev2@gmail.com>
2025-11-17 21:35:21 +0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,31 +1,63 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    CALL silver.load_silver();
+===============================================================================
+*/
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RAISE NOTICE '>> Truncating Table: silver.crm_cust_info'; 
+TRUNCATE TABLE silver.crm_cust_info;
+RAISE NOTICE '>> Inserting Data Into: silver.crm_cust_info';
+
 INSERT INTO silver.crm_cust_info(
 	cst_id, 
 	cst_key, cst_firstname, 
 	cst_lastname, cst_marital_status, 
 	cst_gndr, 
 	cst_create_date)
+	
 SELECT
 cst_id,
 cst_key,
 TRIM(cst_firstname) as cst_firstname,
 TRIM(cst_lastname) as cst_lastname,
-CASE WHEN UPPER(TRIM(cst_marital_status)) IN ('S','SINGLE') THEN 'Single'
+CASE 
+	 WHEN UPPER(TRIM(cst_marital_status)) IN ('S','SINGLE') THEN 'Single'
 	 WHEN UPPER(TRIM(cst_marital_status)) IN ('M', 'MARRIED') THEN 'Married'
 	 ELSE 'N/A'
-END cst_marital_status,
-CASE WHEN UPPER(TRIM(cst_gndr)) IN ('F','FEMALE') THEN 'Female'
+END cst_marital_status, 		-- Normalize marital status values to readable format
+CASE 
+	 WHEN UPPER(TRIM(cst_gndr)) IN ('F','FEMALE') THEN 'Female'
 	 WHEN UPPER(TRIM(cst_gndr)) IN ('M','MALE') THEN 'Male'
-	 ELSE 'N/A'
-END cst_gndr,
+	 ELSE 'N/A' 
+END cst_gndr, 					-- Normalize gender values to readable format
 cst_create_date
 FROM (
 SELECT  
 *,
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC ) AS flag_last
 FROM bronze.crm_cust_info
-)AS T WHERE flag_last = 1 ;
+)AS T WHERE flag_last = 1 ; 	-- Select the most recent record per customer
 
-=======================================================================
+RAISE NOTICE '>> Truncating Table: silver.crm_prd_info'; 
+TRUNCATE TABLE silver.crm_prd_info;
+RAISE NOTICE '>> Inserting Data Into: silver.crm_prd_info'; 
 
 INSERT INTO silver.crm_prd_info(
 prd_id,
@@ -40,8 +72,8 @@ prd_end_dt
 
 SELECT 
 prd_id,
-REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id,
-SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id, -- Extract category ID
+SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,   -- Extract product key
 prd_nm,
 COALESCE(prd_cost,0) AS prd_cost, 
 CASE  UPPER(TRIM(prd_line)) 
@@ -50,12 +82,14 @@ CASE  UPPER(TRIM(prd_line))
 	 WHEN 'S' THEN 'Other Sales'
 	 WHEN 'T' THEN 'Touring'
 	 ELSE 'N/A'
-END AS prd_line,
+END AS prd_line,     		-- Map product line codes to descriptive values
 CAST(prd_start_dt AS DATE) AS prd_start_dt,
-CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 DAY' AS DATE)   AS prd_end_dt
-FROM bronze.crm_prd_info;
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 DAY' AS DATE)   AS prd_end_dt -- Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info; 
 
-=======================================================================
+RAISE NOTICE '>> Truncating Table: silver.crm_sales_details';
+TRUNCATE TABLE silver.crm_sales_details;
+RAISE NOTICE '>> Inserting Data Into: silver.crm_sales_details';
 
 INSERT INTO silver.crm_sales_details (
 sls_ord_num,
@@ -68,70 +102,87 @@ sls_sales,
 sls_quantity,
 sls_price
 )
+	
 SELECT 
 sls_ord_num, 
 sls_prd_key, 
 sls_cust_id,
-CASE WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS VARCHAR)) != 8 THEN NULL
+CASE 
+	 WHEN sls_order_dt = 0 OR LENGTH(CAST(sls_order_dt AS VARCHAR)) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
 END AS sls_order_dt,
 
-CASE WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS VARCHAR)) != 8 THEN NULL
+CASE 
+	 WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS VARCHAR)) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
 END AS sls_ship_dt, 
 
-CASE WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS VARCHAR)) != 8 THEN NULL
+CASE 
+	 WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS VARCHAR)) != 8 THEN NULL
 	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
 END AS sls_due_dt, 
 
-CASE WHEN sls_sales <= 0 OR sls_sales != ABS(sls_price) * ABS(sls_quantity) OR sls_sales IS NULL
+CASE 
+	 WHEN sls_sales <= 0 OR sls_sales != ABS(sls_price) * ABS(sls_quantity) OR sls_sales IS NULL
 	 THEN ABS(sls_price) * ABS(sls_quantity)
 	 ELSE sls_sales
-END AS sls_sales,
+END AS sls_sales,  			-- Recalculate sales if original value is missing or incorrect
 
 sls_quantity,
 
-CASE WHEN sls_price <= 0  OR sls_price IS NULL OR sls_price != ABS(sls_sales) / NULLIF(sls_quantity,0)
+CASE 
+	 WHEN sls_price <= 0  OR sls_price IS NULL OR sls_price != ABS(sls_sales) / NULLIF(sls_quantity,0)
 	 THEN ABS(sls_sales) / NULLIF(sls_quantity,0)
 	 ELSE sls_price
-END AS sls_price
-FROM bronze.crm_sales_details
+END AS sls_price    		-- Derive price if original value is invalid
+FROM bronze.crm_sales_details;
 	
-=======================================================================
+RAISE NOTICE '>> Truncating Table: silver.erp_cust_az12';
+TRUNCATE TABLE silver.erp_cust_az12;
+RAISE NOTICE '>> Inserting Data Into: silver.erp_cust_az12';
 
 INSERT INTO silver.erp_cust_az12(cid,
 bdate,
 gen
 )
+	
 SELECT 
 TRIM(REPLACE(cid,'NAS','')) AS cid,
 
-CASE WHEN bdate > CURRENT_DATE THEN NULL
+CASE 
+	 WHEN bdate > CURRENT_DATE THEN NULL
 	 ELSE bdate
-END AS bdate,
+END AS bdate,  -- Derive price if original value is invalid
 
-CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+CASE 
+	 WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
 	 WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
 	 ELSE 'N/A'
-END AS gen
-FROM bronze.erp_cust_az12
+END AS gen   				-- Normalize gender values and handle unknown cases
+FROM bronze.erp_cust_az12;
 	
-=======================================================================
+RAISE NOTICE '>> Truncating Table: silver.erp_loc_a101';
+TRUNCATE TABLE silver.erp_loc_a101;
+RAISE NOTICE '>> Inserting Data Into: silver.erp_loc_a101';
 
 INSERT INTO silver.erp_loc_a101(
 cid,
 cntry
 )
+	
 SELECT 
 REPLACE(cid,'-','') AS  cid,
-CASE WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+CASE 
+	 WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
  	 WHEN TRIM(cntry) = 'DE' THEN 'Germany'
 	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
 	 ELSE TRIM(cntry)
-END AS cntry
-FROM bronze.erp_loc_a101
+END AS cntry  			 -- Normalize and Handle missing or blank country codes
+FROM bronze.erp_loc_a101;
 
-=======================================================================
+RAISE NOTICE '>> Truncating Table: silver.erp_px_cat_g1v2'; 
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+RAISE NOTICE '>> Inserting Data Into: silver.erp_px_cat_g1v2';
 
 INSERT INTO silver.erp_px_cat_g1v2(
 id,
@@ -139,11 +190,14 @@ cat,
 subcat,
 maintenance
 )
+	
 SELECT 
 TRIM(id),
 TRIM(cat),
 TRIM(subcat),
 TRIM(maintenance)
-FROM bronze.erp_px_cat_g1v2
+FROM bronze.erp_px_cat_g1v2;
+END;
+$$;
 
 
